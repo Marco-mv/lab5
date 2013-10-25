901058ef7f86752f1920f93fe542603a973d2366
unknown <iguest@IS-M334L23-21.ischool.uw.edu>
2013-10-25 13:26:50 -0700

    lab 5 in class

--- js/address-book.js
+++ js/address-book.js
@@ -2,18 +2,8 @@
     this is where you will add your JavaScript to complete Lab 5
 */
 
+$(pageLoad);
 
-/* sortObjArray()
-    sorts an array of objects by a given property name
-    the property values are compared using standard 
-    operators, so this will work for string, numeric,
-    boolean, or date values
-
-    objArray        array of objects to sort
-    propName        property name to sort by
-
-    returns undefined (array is sorted in place)
-*/
 function sortObjArray(objArray, propName) {
     if (!objArray.sort)
         throw new Error('The objArray parameter does not seem to be an array (no sort method)');
@@ -33,4 +23,73 @@ function sortObjArray(objArray, propName) {
             return 1;
     });
 } //sortObjArray()
+
+
+
+function pageLoad(){
+    var arr = Employees.entries;
+    sortObjArray(arr, 'last');
+    render(arr);
+}
+
+function render(entries){
+    var template = $('.template');
+    var address = $('.address-book');
+    address.empty();
+
+    var instance;
+
+    $.each(Employees.entries, function(){
+       instance = template.clone();
+       for (prop in this) {
+            if (prop === 'pic') {
+                instance.find('.pic').attr({
+                    src: this[prop],
+                    alt: 'Picture of ' + this[prop]
+                });
+            } else { 
+                instance.find('.' + prop).html(this[prop]);
+        }
+
+        instance.removeClass('template');
+        address.append(instance);
+	   }
+	});
+        
+}
+
+$('.sort-ui .btn').click(buttonPush);
+function buttonPush(){
+    var sortBtn = $(this);
+    var buttonName = sortBtn.attr('data-sortby');
+    sortObjArray(Employees.entries, buttonName);
+    $('.btn').removeClass('active');
+    sortBtn.addClass('active');
+    render(Employees.entries);
+}
+
+
+/* sortObjArray()
+    sorts an array of objects by a given property name
+    the property values are compared using standard 
+    operators, so this will work for string, numeric,
+    boolean, or date values
+
+    objArray        array of objects to sort
+    propName        property name to sort by
+
+    returns undefined (array is sorted in place)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
